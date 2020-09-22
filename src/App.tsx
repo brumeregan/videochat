1d718cd0a563213ab9569c6b8093cf48db946518
Oksana Boyarko <oboyarko@danavero.com>
2020-09-22 18:32:14 +0200

    add call functionality for local calls

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import React from 'react';
-import { Counter } from './components/features/counter/Counter';
 import {
   Switch,
-  Route,
-   Link
+  Route
 } from "react-router-dom";
-import {Inner} from './components/features/inner/Inner';
-import s from './App.module.scss';
 import {ChangeLanguage} from './components/features/changeLanguage/ChangeLanguage';
+import { VideoPage } from './components/features/videoPage/VideoPage';
 
 function App() {
-console.log(s)
   return (
     <>
-    <div className={s.root}>
-      <Link to="/">Home</Link>
-      <Link to="/inner">Inner page</Link>
-
+    <div>
       <ChangeLanguage />
     </div>
     <Switch>
         <Route path="/" exact>
-            <Counter />
-        </Route>
-        <Route path="/inner">
-            <Inner />
+            <VideoPage />
         </Route>
     </Switch>
     </>
---
+++ src/components/features/videoPage/helpers.tsx
@@ -0,0 +1,18 @@
+
+export const handleConnectionSuccess = () => {
+    console.log(`addIceCandidate success.`);
+};
+
+export const setSessionDescriptionError = (error: Error) => {
+    console.log(`Failed to create session description: ${error.toString()}.`);
+}
+
+export const setDescriptionSuccess = (functionName: any) => {
+    console.log(`${functionName} complete.`);
+}
+
+export const handleConnectionFailure = (error: Error) => {
+    console.log(`failed to add ICE Candidate:\n`+
+          `${error.toString()}.`);
+
+  }
